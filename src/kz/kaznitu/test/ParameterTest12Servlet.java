4f9e931d282392b093f072c8e80887e808568c78
Aigerym Faizulla <askarovnaa97@gmail.com>
2017-10-23 11:15:25 +0600

    Бірінші коммит курылды

---
+++ src/kz/kaznitu/test/ParameterTest12Servlet.java
@@ -0,0 +1,31 @@
+package kz.kaznitu.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class ParameterTest12Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        String title = "Using Get Method to Read Form Data";
+        String docType = "<!docType html>\n";
+        out.println(docType +
+                " <html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor = \"#f0f0f0\">\n " +
+                "<h1 align = \"center\" >" + title  + "</h1>\n"+
+                "<ul>\n" +
+                " <li> <b> First Name </b>: " + req.getParameter("first name") + "\n"+
+                " <li> <b> Last NAme </b>: " + req.getParameter("last name") + "\n" +
+                " </ul>\n " +
+                "</body>\n" +
+                "</html>" );
+    }
+}
+ 
